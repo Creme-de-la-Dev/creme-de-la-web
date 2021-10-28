2e6e2f9c4b6429c40735b267e4a2e54bb607ae2e
Guilherme Chan <guilhermechan30@gmail.com>
2021-10-28 16:08:27 -0300

    Navbar created (desktop version only)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Headers from './Headers';
+import Footer from './Footer';
+import ale from './images/uale.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Headers />
+      <div className="container">
+        <h1>Desenvolvendo u ale</h1>
+        <img src={ale} alt="o ale." />
+      </div>
     </div>
   );
 }
---
+++ src/Headers.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './App.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const Headers = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+        <div>
+      <Navbar
+        expand="md"
+      >
+        <NavbarBrand href="/">
+          Creme de la Nage
+        </NavbarBrand>
+        <NavbarToggler onClick={function noRefCheck(){}} />
+        <Collapse navbar>
+          <Nav
+            className="mr-auto"
+            navbar
+          >
+            <NavItem>
+              <NavLink href="/components/">
+                Séries
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                Jogos
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                Merch
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                Sobre nós
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+      )
+}
+
+export default Headers;
