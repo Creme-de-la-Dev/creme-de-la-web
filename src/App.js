3fba51ac758cbdc964c436055d09e69d77a8c8b3
Guilherme Chan <guilhermechan30@gmail.com>
2021-11-03 12:43:36 -0300

    animations added using animate.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'animate.css';
 
 import Headers from './Headers';
 import Footer from './Footer';
@@ -14,14 +15,14 @@ function App() {
       <div className="container">
         <div className="home-header">
           <div className="title-and-btn">
-            <div className="title"><h1>Todo conteúdo que você adora, agora em um só lugar.</h1></div>
+            <div className="title"><h1 className="animate__animated animate__fadeInLeft">Todo conteúdo que você adora, agora em um só lugar.</h1></div>
             <div>
-              <Button>
+              <Button className="animate__animated animate__fadeInLeft">
                  <FaArrowCircleDown className="arrow-icon"/>  Explore a página
               </Button>
             </div>
           </div>
-          <img src={ale} alt="o ale." className="ale-img"/>
+            <img src={ale} alt="o ale." className="ale-img animate__animated animate__fadeIn"/>
         </div>
       </div>
     </div>
