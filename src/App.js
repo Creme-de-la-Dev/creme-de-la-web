dcde91f368f1c9803966948e8d7b8f92173c00b9
Guilherme Chan <guilhermechan30@gmail.com>
2021-11-04 15:33:47 -0300

    cards design done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+// Style
 import './App.css';
 import 'animate.css';
 
+// Components
 import Headers from './Headers';
 import Footer from './Footer';
 
-import { Button } from 'reactstrap';
-import { FaArrowCircleDown, FaPlayCircle } from 'react-icons/fa';
-
+// Images
 import ale from './images/uale.png';
 import series from './images/series-svg.svg';
+import games from './images/games-svg.svg';
+import merch from './images/merch-svg.svg';
 
+// ReactStrap
+import { Button } from 'reactstrap';
+
+// Icons
+import { FaArrowCircleDown, FaPlayCircle, FaGamepad, FaShoppingCart } from 'react-icons/fa';
 
 
 function App() {
@@ -19,8 +26,12 @@ function App() {
       <div className="container">
         <div className="home-header">
           <div className="title-and-btn">
-            <div className="title"><h1 className="animate__animated animate__fadeInLeft">Todo conteúdo que você adora, agora em um só lugar.</h1></div>
-            <div>
+            <div className="title">
+              <h1 className="animate__animated animate__fadeInLeft title-class">
+                Todo conteúdo que você adora, agora em um só lugar.
+              </h1>
+            </div>
+          <div>
               <Button className="orange-cta-btn animate__animated animate__fadeInLeft">
                  <FaArrowCircleDown className="arrow-icon"/>  Explore a página
               </Button>
@@ -31,16 +42,42 @@ function App() {
       </div>
       <section className="content">
         <div className="main">
-          <div className="container-card">
-            <img className="imag" src={series} alt="series" />
-            <div className="text-container">
-              <h1>Séries Aclamadas</h1>
-              <p>
-              Webshows do grupo Creme de la Nage já estão disponíveis na plataforma, sendo estes Luan’s Revolution (2015), Creme de la Nage TV (2016) e Turma da Válvula (2021).
-              </p>
-              <Button className="green-cta-btn">
-                <FaPlayCircle className="play-icon" /> Assista Agora
-              </Button>
+          <div className="cards">
+            <div className="container-card animate__animated animate__fadeInUp">
+              <img className="imag" src={series} alt="series" />
+              <div className="text-container">
+                <h1>Séries Aclamadas</h1>
+                <p>
+                  Webshows do grupo Creme de la Nage já estão disponíveis na plataforma, sendo estes Luan’s Revolution (2015), Creme de la Nage TV (2016) e Turma da Válvula (2021).
+                </p>
+                <Button className="green-cta-btn">
+                  <FaPlayCircle className="play-icon" size = '10x'/> Assista Agora
+                </Button>
+              </div>
+            </div>
+            <div className="container-card animate__animated animate__fadeInUp">
+              <img className="imag" src={games} alt="games" />
+              <div className="text-container">
+                <h1>Jogos Divertidos</h1>
+                <p>
+                  Jogos de navegador criaddos pela equipe Creme de la Nage estão disponíveis, sendo estes Flappy Catito, Lorax Kart e futuramente, Cremeball.
+                </p>
+                <Button className="green-cta-btn">
+                  <FaGamepad className="game-icon" /> Jogar Agora
+                </Button>
+              </div>
+            </div>
+            <div className="container-card animate__animated animate__fadeInUp">
+              <img className="imag" src={merch} alt="merch" />
+              <div className="text-container">
+                <h1>Brinquedos e acessórios</h1>
+                <p>
+                  Novidade! Agora o Creme de la Nage tem sua própria loja! Compre nossos itens, camisetas, canecas e brinquedos com frete grátis para todo o Brasil!
+                </p>
+                <Button className="green-cta-btn">
+                  <FaShoppingCart className="merch-icon"/> Comprar Agora
+                </Button>
+              </div>
             </div>
           </div>
         </div>
