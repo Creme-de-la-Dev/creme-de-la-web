9fa4c9bdffe7c0ccde013909179f9712af0dfc37
Guilherme Chan <guilhermechan30@gmail.com>
2021-11-04 13:52:40 -0300

    series card done

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Headers from './Headers';
 import Footer from './Footer';
 
 import { Button } from 'reactstrap';
-import { FaArrowCircleDown } from 'react-icons/fa';
+import { FaArrowCircleDown, FaPlayCircle } from 'react-icons/fa';
+
 import ale from './images/uale.png';
+import series from './images/series-svg.svg';
+
+
 
 function App() {
   return (
@@ -17,7 +21,7 @@ function App() {
           <div className="title-and-btn">
             <div className="title"><h1 className="animate__animated animate__fadeInLeft">Todo conteúdo que você adora, agora em um só lugar.</h1></div>
             <div>
-              <Button className="animate__animated animate__fadeInLeft">
+              <Button className="orange-cta-btn animate__animated animate__fadeInLeft">
                  <FaArrowCircleDown className="arrow-icon"/>  Explore a página
               </Button>
             </div>
@@ -25,6 +29,22 @@ function App() {
             <img src={ale} alt="o ale." className="ale-img animate__animated animate__fadeIn"/>
         </div>
       </div>
+      <section className="content">
+        <div className="main">
+          <div className="container-card">
+            <img className="imag" src={series} alt="series" />
+            <div className="text-container">
+              <h1>Séries Aclamadas</h1>
+              <p>
+              Webshows do grupo Creme de la Nage já estão disponíveis na plataforma, sendo estes Luan’s Revolution (2015), Creme de la Nage TV (2016) e Turma da Válvula (2021).
+              </p>
+              <Button className="green-cta-btn">
+                <FaPlayCircle className="play-icon" /> Assista Agora
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
