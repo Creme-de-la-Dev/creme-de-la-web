eaf1b9a2cc1ad9c982568e003c1ca4da0df81731
gchan26 <guilhermechan30@gmail.com>
2023-03-12 01:32:32 -0300

    Started creating main section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,33 @@
 // React
-import React from 'react';
+import React from "react";
 
-function Home () {
-    return (
-        <div className="bg-gradient-to-b from-[#265881] to-[#265881] w-screen h-screen">
-            <div className="app">
-                <h1>Creme de la Web Home</h1>
+// Icons
+import { BsArrowDownCircleFill } from 'react-icons/bs';
+
+function Home() {
+  return (
+    <div className="bg-gradient-to-b from-[#265881] to-[#265881] w-screen h-screen">
+      <div className="container p-8">
+        <div className="p-20 flex flex-col gap-5">
+            <h1 className="text-8xl text-cdln-blue-100 font-medium">Creme de la Web</h1>
+            <h2 className="text-3xl text-slate-400 font-light">
+            Todo conteúdo do Creme que você adora,
+            <br />
+            agora em um só lugar
+            </h2>
+            <div className="pt-5">
+                <button className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow flex flex-row items-center gap-2 text-2xl">
+                    <BsArrowDownCircleFill />
+                    Explorar conteúdo
+                </button>
             </div>
         </div>
-    )
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
