2548b8773a92c4997e82d37c78d3e5d0b2447cc1
gchan26 <guilhermechan30@gmail.com>
2023-03-16 16:43:58 -0300

    Added waves animation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,28 +7,31 @@ import { BsArrowDownCircleFill } from "react-icons/bs";
 // Images
 import Logo from "../../assets/ale-logo.png";
 
+// Animations
+import "animate.css";
+
 function Home() {
   return (
     <div>
       <section className="bg-gradient-to-b from-[#265881] to-[#2D679C] h-screen">
-        <div className="md:p-8 flex flex-col md:flex-row justify-between w-full items-center">
+        <div className="md:p-8 xl:p-4 flex flex-col md:flex-row justify-between w-full items-center">
           <div className="md:p-20 p-10 flex flex-col gap-5">
-            <h1 className="text-5xl w-full md:text-8xl text-cdln-blue-100 font-medium text-center">
+            <h1 className="text-5xl w-full md:text-8xl text-cdln-blue-100 font-medium text-center animate__animated animate__fadeInLeft">
               Creme de la Web
             </h1>
-            <h2 className="md:text-3xl text-lg justify-center text-slate-400 font-light">
+            <h2 className="md:text-3xl text-lg justify-center text-slate-400 font-light animate__animated animate__fadeInLeft">
               Todo conteúdo do Creme que você adora,
               <br />
               agora em um só lugar
             </h2>
-            <div className="pt-5 justify-center self-center md:self-auto">
+            <div className="pt-5 justify-center self-center md:self-auto animate__animated animate__fadeIn">
               <button className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-2 text-2xl">
                 <BsArrowDownCircleFill />
                 Explorar conteúdo
               </button>
             </div>
           </div>
-          <div className="flex flex-col md:p-20 mt-10 md:mt-0">
+          <div className="flex flex-col md:p-20 mt-10 md:mt-0 animate__animated animate__pulse">
             <img
               className="h-64 w-64 md:h-full md:w-full rounded-full border-2 border-solid border-white shadow-xl"
               src={Logo}
@@ -37,8 +40,12 @@ function Home() {
           </div>
         </div>
       </section>
-      <section class="bg-gradient-to-b from-[#2D679C] to-[#3982C2] h-screen">
-
+      <section class="bg-cdln-blue-900 h-screen">
+        <div class="ocean">
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>
+        </div>
       </section>
     </div>
   );
