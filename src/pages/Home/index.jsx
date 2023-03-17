b190777e895a384279e8fef469320c8dad0d1354
gchan26 <guilhermechan30@gmail.com>
2023-03-16 23:46:43 -0300

    Finished main section design

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,6 @@ import React from "react";
 import { BsArrowDownCircleFill } from "react-icons/bs";
 import { IoGameController } from "react-icons/io5";
 import {
-  HiHome,
   HiFilm,
   HiShoppingBag,
 } from "react-icons/hi";
@@ -49,13 +48,14 @@ function Home() {
           </div>
         </div>
       </section>
-      <section className="bg-cdln-blue-900 h-screen" id="main-section">
+      <section className="bg-cdln-blue-900" id="main-section">
         <div className="ocean">
           <div className="wave"></div>
           <div className="wave"></div>
           <div className="wave"></div>
         </div>
-        <div class="pt-28">
+        {/* GAMES */}
+        <div class="pt-44">
           <div className="games">
             <div className="flex flex-col sm:flex-row justify-center sm:justify-evenly p-4 sm:p-0">
               <img className="w-64 md:w-80 lg:w-96 self-center" src={Game} alt="Game" />
@@ -76,13 +76,17 @@ function Home() {
             </div>
           </div>
         </div>
+        {/* SHOWS */}
         <div class="pt-44">
-          <div className="games">
+          <div className="shows">
             <div className="flex flex-col sm:flex-row justify-center sm:justify-evenly p-4 sm:p-0">
+            <img className="w-64 md:w-80 lg:w-96 self-center sm:hidden" src={Shows} alt="Shows" />
               <div className="flex flex-col gap-7">
                 <h1 className="font-semibold text-5xl w-full md:text-7xl lg:text-8xl text-cdln-blue-100 text-center">Séries Aclamadas</h1>
                 <h2 className="lg:text-3xl md:text-2xl text-lg justify-center text-slate-400 font-light text-center">
-                Webshows do grupo Creme de la Nage já estão disponíveis na plataforma, sendo estes Luan’s Revolution (2015),<br/>Creme de la Nage TV (2016), Turma da Válvula (2021) e Luan's Revolution: Aftermath (2023).
+                Webshows do grupo <strong>Creme de la Nage</strong> já estão disponíveis na plataforma, sendo estes <strong>Luan’s Revolution (2015)</strong>,
+                <br/>
+                <strong>Creme de la Nage TV (2016)</strong>, <strong>Turma da Válvula (2021)</strong> e <strong>Luan's Revolution: Aftermath (2023)</strong>.
                 </h2>
                 <div className="pt-5 self-center animate__animated animate__fadeIn">
                   <button className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-3 text-2xl" href="/">
@@ -91,7 +95,27 @@ function Home() {
                   </button>
                 </div>
               </div>
-              <img className="w-64 md:w-80 lg:w-96 self-center" src={Shows} alt="Shows" />
+              <img className="w-64 md:w-80 lg:w-96 self-center sm:block hidden" src={Shows} alt="Shows" />
+            </div>
+          </div>
+        </div>
+        {/* STORE */}
+        <div class="py-44">
+          <div className="store">
+            <div className="flex flex-col sm:flex-row justify-center sm:justify-evenly p-4 sm:p-0">
+              <img className="w-64 md:w-80 lg:w-96 self-center" src={Store} alt="Store" />
+              <div className="flex flex-col gap-7">
+                <h1 className="font-semibold text-5xl w-full md:text-7xl lg:text-8xl text-cdln-blue-100 text-center">Creme Store</h1>
+                <h2 className="lg:text-3xl md:text-2xl text-lg justify-center text-slate-400 font-light text-center">
+                Compre já nossos itens, camisetas, canecas e brinquedos do Creme de la Nage<br/>com frete grátis para todo o Brasil!
+                </h2>
+                <div className="pt-5 self-center animate__animated animate__fadeIn">
+                  <button className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-3 text-2xl" href="/">
+                    <HiShoppingBag size={30} />
+                    Conferir Loja
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
