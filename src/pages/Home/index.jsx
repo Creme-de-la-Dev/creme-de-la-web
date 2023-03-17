7aa96d491965cf478bb4e211577165e8d5cd7327
gchan26 <guilhermechan30@gmail.com>
2023-03-16 23:58:42 -0300

    Finished Home Page Design

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,10 +4,7 @@ import React from "react";
 // Icons
 import { BsArrowDownCircleFill } from "react-icons/bs";
 import { IoGameController } from "react-icons/io5";
-import {
-  HiFilm,
-  HiShoppingBag,
-} from "react-icons/hi";
+import { HiFilm, HiShoppingBag } from "react-icons/hi";
 
 // Images
 import Logo from "../../assets/ale-logo.png";
@@ -33,7 +30,10 @@ function Home() {
               agora em um só lugar
             </h2>
             <div className="pt-5 justify-center self-center md:self-auto animate__animated animate__fadeIn">
-              <button className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-2 text-2xl" href="#main-section">
+              <button
+                className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-2 text-2xl"
+                href="#main-section"
+              >
                 <BsArrowDownCircleFill />
                 Explorar conteúdo
               </button>
@@ -56,18 +56,30 @@ function Home() {
         </div>
         {/* GAMES */}
         <div class="pt-44">
-          <div className="games">
+          <div className="games animate__animated animate__zoomIn">
             <div className="flex flex-col sm:flex-row justify-center sm:justify-evenly p-4 sm:p-0">
-              <img className="w-64 md:w-80 lg:w-96 self-center" src={Game} alt="Game" />
+              <img
+                className="w-64 md:w-80 lg:w-96 self-center"
+                src={Game}
+                alt="Game"
+              />
               <div className="flex flex-col gap-7">
-                <h1 className="font-semibold text-5xl w-full md:text-7xl lg:text-8xl text-cdln-blue-100 text-center">Jogos Divertidos</h1>
+                <h1 className="font-semibold text-5xl w-full md:text-7xl lg:text-8xl text-cdln-blue-100 text-center">
+                  Jogos Divertidos
+                </h1>
                 <h2 className="lg:text-3xl md:text-2xl text-lg justify-center text-slate-400 font-light text-center">
-                Jogos fantásticos totalmente pensados e feitos pela equipe <strong>Creme de la Nage</strong> estão disponíveis,
-                <br />
-                sendo estes <strong>Flappy Catito</strong>, <strong>Lorax Kart</strong> e futuramente contará com diversos jogos incríveis!
+                  Jogos fantásticos totalmente pensados e feitos pela equipe{" "}
+                  <strong>Creme de la Nage</strong> estão disponíveis,
+                  <br />
+                  sendo estes <strong>Flappy Catito</strong>,{" "}
+                  <strong>Lorax Kart</strong> e futuramente contará com diversos
+                  jogos incríveis!
                 </h2>
                 <div className="pt-5 self-center animate__animated animate__fadeIn">
-                  <button className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-3 text-2xl" href="/">
+                  <button
+                    className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-3 text-2xl"
+                    href="/"
+                  >
                     <IoGameController size={30} />
                     Conferir os jogos
                   </button>
@@ -78,39 +90,68 @@ function Home() {
         </div>
         {/* SHOWS */}
         <div class="pt-44">
-          <div className="shows">
+          <div className="shows animate__animated animate__zoomIn">
             <div className="flex flex-col sm:flex-row justify-center sm:justify-evenly p-4 sm:p-0">
-            <img className="w-64 md:w-80 lg:w-96 self-center sm:hidden" src={Shows} alt="Shows" />
+              <img
+                className="w-64 md:w-80 lg:w-96 self-center sm:hidden"
+                src={Shows}
+                alt="Shows"
+              />
               <div className="flex flex-col gap-7">
-                <h1 className="font-semibold text-5xl w-full md:text-7xl lg:text-8xl text-cdln-blue-100 text-center">Séries Aclamadas</h1>
+                <h1 className="font-semibold text-5xl w-full md:text-7xl lg:text-8xl text-cdln-blue-100 text-center">
+                  Séries Aclamadas
+                </h1>
                 <h2 className="lg:text-3xl md:text-2xl text-lg justify-center text-slate-400 font-light text-center">
-                Webshows do grupo <strong>Creme de la Nage</strong> já estão disponíveis na plataforma, sendo estes <strong>Luan’s Revolution (2015)</strong>,
-                <br/>
-                <strong>Creme de la Nage TV (2016)</strong>, <strong>Turma da Válvula (2021)</strong> e <strong>Luan's Revolution: Aftermath (2023)</strong>.
+                  Webshows do grupo <strong>Creme de la Nage</strong> já estão
+                  disponíveis na plataforma, sendo estes{" "}
+                  <strong>Luan’s Revolution (2015)</strong>,
+                  <br />
+                  <strong>Creme de la Nage TV (2016)</strong>,{" "}
+                  <strong>Turma da Válvula (2021)</strong> e{" "}
+                  <strong>Luan's Revolution: Aftermath (2023)</strong>.
                 </h2>
                 <div className="pt-5 self-center animate__animated animate__fadeIn">
-                  <button className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-3 text-2xl" href="/">
+                  <button
+                    className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-3 text-2xl"
+                    href="/"
+                  >
                     <HiFilm size={30} />
                     Conferir as séries
                   </button>
                 </div>
               </div>
-              <img className="w-64 md:w-80 lg:w-96 self-center sm:block hidden" src={Shows} alt="Shows" />
+              <img
+                className="w-64 md:w-80 lg:w-96 self-center sm:block hidden"
+                src={Shows}
+                alt="Shows"
+              />
             </div>
           </div>
         </div>
         {/* STORE */}
         <div class="py-44">
-          <div className="store">
+          <div className="store animate__animated animate__zoomIn">
             <div className="flex flex-col sm:flex-row justify-center sm:justify-evenly p-4 sm:p-0">
-              <img className="w-64 md:w-80 lg:w-96 self-center" src={Store} alt="Store" />
+              <img
+                className="w-64 md:w-80 lg:w-96 self-center"
+                src={Store}
+                alt="Store"
+              />
               <div className="flex flex-col gap-7">
-                <h1 className="font-semibold text-5xl w-full md:text-7xl lg:text-8xl text-cdln-blue-100 text-center">Creme Store</h1>
+                <h1 className="font-semibold text-5xl w-full md:text-7xl lg:text-8xl text-cdln-blue-100 text-center">
+                  Creme Store
+                </h1>
                 <h2 className="lg:text-3xl md:text-2xl text-lg justify-center text-slate-400 font-light text-center">
-                Compre já nossos itens, camisetas, canecas e brinquedos do Creme de la Nage<br/>com frete grátis para todo o Brasil!
+                  Compre já nossos itens, camisetas, canecas e brinquedos do
+                  Creme de la Nage
+                  <br />
+                  com frete grátis para todo o Brasil!
                 </h2>
                 <div className="pt-5 self-center animate__animated animate__fadeIn">
-                  <button className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-3 text-2xl" href="/">
+                  <button
+                    className="bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg flex flex-row items-center gap-3 text-2xl"
+                    href="/"
+                  >
                     <HiShoppingBag size={30} />
                     Conferir Loja
                   </button>
