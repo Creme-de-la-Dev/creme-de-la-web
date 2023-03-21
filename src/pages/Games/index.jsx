7a8ebcc0eb1e0d85b485791db139bf32baf9d155
gchan26 <guilhermechan30@gmail.com>
2023-03-20 23:04:09 -0300

    Added animations

--- src/pages/Games/index.jsx
+++ src/pages/Games/index.jsx
@@ -8,25 +8,37 @@ import ArrowDown from "../../assets/arrow-down.svg";
 function Games() {
   return (
     <div>
-        <div className="header h-screen">
-            <div className="h-full w-full relative">
-                <img
-                src={GamesBG}
-                className="w-full h-full object-cover absolute mix-blend-overlay"
-                />
-                <div className="p-24 w-full h-full justify-center items-center">
-                <h1 className="text-cdln-blue-400 text-9xl font-bold text-center animate__animated animate__jackInTheBox">
-                    Jogos online do Creme
-                </h1>
-                <h2 className="text-cdln-blue-300 text-5xl font-light mt-5 text-center animate__animated animate__slideInUp">
-                    Diversão Garantida!
-                </h2>
-                <div className="w-full justify-center items-center pt-20 animate__animated animate__fadeIn">
-                    <img src={ArrowDown} className="mx-auto h-10 w-10"/>
-                </div>
-                </div>
+      <section className="header h-screen">
+        <div className="h-full w-full relative">
+          <img
+            src={GamesBG}
+            className="w-full h-full object-cover absolute mix-blend-overlay"
+          />
+          <div className="p-24 w-full h-full justify-center items-center">
+            <h1 className="text-cdln-blue-400 text-9xl font-bold text-center animate__animated animate__jackInTheBox">
+              Jogos online do Creme
+            </h1>
+            <h2 className="text-cdln-blue-300 text-5xl font-light mt-5 text-center animate__animated animate__slideInUp">
+              Diversão Garantida!
+            </h2>
+            <div className="w-full justify-center items-center pt-20 animate__animated animate__fadeIn">
+              <a href="#main-section">
+                <img src={ArrowDown} className="mx-auto h-10 w-10" />
+              </a>
             </div>
+          </div>
         </div>
+      </section>
+      <section className="h-screen" id="main-section">
+      <div class="hero"> 
+        <div class="cube"></div>
+        <div class="cube"></div>
+        <div class="cube"></div>
+        <div class="cube"></div>
+        <div class="cube"></div>
+        <div class="cube"></div>
+        </div>
+      </section>
     </div>
   );
 }
