ce694ef105fde3930485c825917aae0a2678a2a7
gchan26 <guilhermechan30@gmail.com>
2023-03-20 21:50:38 -0300

    Started modifing layout

--- src/pages/Games/index.jsx
+++ src/pages/Games/index.jsx
@@ -1,17 +1,28 @@
 // React
-import React from 'react';
+import React from "react";
 
 // Images
 import GamesBG from "../../assets/GamesBG.png";
 
-function Games () {
-    return (
-        <div className="app h-screen">
-            <div className='bg-gradient-to-tl from-purple-900 to-green-700 h-full w-full relative'>
-                <img src={GamesBG} className="w-full h-full object-cover absolute mix-blend-overlay" />
-            </div>
+function Games() {
+  return (
+    <div className="app h-screen">
+      <div className="h-full w-full relative">
+        <img
+          src={GamesBG}
+          className="w-full h-full object-cover absolute mix-blend-overlay"
+        />
+        <div className="p-24 w-full h-full justify-center items-center">
+          <h1 className="text-cdln-blue-400 text-9xl font-bold text-center">
+            Jogos online do Creme
+          </h1>
+          <h2 className="text-blue-300 text-5xl font-light mt-5 text-center">
+            Diversão Garantida!
+          </h2>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Games;
