fc3ac7be30c4f5ecd6b61c4d821de32f7e4c2853
gchan26 <guilhermechan30@gmail.com>
2023-03-21 22:39:30 -0300

    Fixed lorax card

--- src/pages/Games/index.jsx
+++ src/pages/Games/index.jsx
@@ -7,6 +7,9 @@ import GamesBGsm from "../../assets/GamesBG-sm.png";
 import ArrowDown from "../../assets/arrow-down.svg";
 import LoraxKart from "../../assets/LORAX-KART-PROMO.png";
 
+// Icons
+import { IoGameController } from "react-icons/io5";
+
 function Games() {
   return (
     <div>
@@ -44,8 +47,8 @@ function Games() {
           <div className="cube"></div>
           <div className="cube"></div>
           <div>
-            <div className="p-10">
-              <article className="shadow-lg mx-auto relative max-w-md group cursor-pointer">
+            <div className="flex flex-col md:flex-row p-10">
+              <article className="relative max-w-md group shadow-md mx-auto transform hover:-translate-y-1 duration-300 hover:shadow-xl cursor-pointer">
                 <div className="overflow-hidden">
                   <img
                     className="w-full h-auto transform hover:scale-110 duration-200"
@@ -53,17 +56,19 @@ function Games() {
                     alt="Lorax Kart"
                   />
                 </div>
-                <div className="p-7 my-auto pb-12 ">
+                <div className="p-7 my-auto pb-12 bg-cdln-blue-50">
                   <h1 className="text-4xl font-semibold text-gray-800 mt-4">
-                    Tropicalia
+                    Lorax Kart
                   </h1>
                   <p className="text-2xl text-gray-400 mt-4 leading-relaxed">
-                    Super creative and colorful illustrations by Matheus Lopes.
-                    Check out more of his amazing artworks in his portfolio.
+                    Aperte os cintos! Embarque com Lorax nesse grande desafio
+                    cheio de adrenalina na grande São Paulo e busque o Flávio no
+                    aeroporto de Guarulhos a tempo.
                   </p>
                 </div>
-                <div className="border-t pt-8 pb-8 text-center text-base text-gray-400 uppercase tracking-widest  group-hover:text-gray-600 bg-gray-50">
-                  Matheus Lopes
+                <div className="flex gap-3 justify-center border-t py-8 items-center text-cdln-blue-200 group-hover:text-cdln-blue-50 bg-cdln-blue-600 hover:bg-cdln-blue-700">
+                  <IoGameController size={"1.5em"}/>
+                  <h2 className="text-center text-xl tracking-widest uppercase">Jogar</h2>
                 </div>
               </article>
             </div>
