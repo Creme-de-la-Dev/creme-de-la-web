02210a0a18c1608ab254d1facdf88ed23055f12d
Luan <barkon32@tuta.io>
2023-03-23 18:39:14 -0300

    Game cards are now components

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,42 @@
+import { IoGameController } from "react-icons/io5";
+
+export default function GameCard(props) {
+  const {available = true} = props;
+  return (
+    <article className="rounded-md overflow-hidden relative max-w-sm group shadow-md mx-auto transform hover:-translate-y-1 duration-300 hover:shadow-xl cursor-pointer">
+      <div className="overflow-hidden">
+        <img
+          className="w-full h-auto transform hover:scale-110 duration-200"
+          src={props.image}
+          alt={props.name}
+        />
+      </div>
+      <div className="p-7 my-auto pb-12 bg-cdln-blue-50">
+        <h1 className="text-4xl font-semibold text-gray-800 mt-4">
+          {props.name}
+        </h1>
+        <p className="text-lg md:text-2xl text-gray-400 mt-4 leading-relaxed">
+          <span dangerouslySetInnerHTML={{ __html: props.description }}></span>
+        </p>
+      </div>
+      {available ? (
+      <a href={props.gameUrl} target="_blank">
+        <div className="flex gap-3 justify-center border-t py-8 items-center text-cdln-blue-200 group-hover:text-cdln-blue-50 bg-cdln-blue-600 hover:bg-cdln-blue-700">
+          <IoGameController size={"1.5em"} />
+          <h2 className="text-center text-xl tracking-widest uppercase">
+            Jogar
+          </h2>
+        </div>
+      </a>
+      ) : (
+      <a href="https://youtu.be/mc5ub3TULqU?t=16" target="_blank">
+        <div className="flex gap-3 justify-center border-t py-8 items-center text-cdln-blue-50 bg-gray-500">
+          <h2 className="text-center text-xl tracking-widest uppercase">
+            Em Breve
+          </h2>
+        </div>
+      </a>
+      )}
+    </article>
+  );
+}
--- src/pages/Games/index.jsx
+++ src/pages/Games/index.jsx
@@ -13,6 +13,8 @@ import Akon from "../../assets/AKON-PROMO.png";
 import { IoGameController } from "react-icons/io5";
 import { MdPhonelinkErase } from "react-icons/md";
 
+import GameCard from "../../components/GameCard";
+
 function Games() {
   return (
     <div>
@@ -51,95 +53,30 @@ function Games() {
           <div className="cube"></div>
           <div>
             <div className="flex flex-col lg:flex-row p-5 lg:p-20 gap-8 lg:gap-0">
-              <article className="rounded-md overflow-hidden relative max-w-sm group shadow-md mx-auto transform hover:-translate-y-1 duration-300 hover:shadow-xl cursor-pointer">
-                <div className="overflow-hidden">
-                  <img
-                    className="w-full h-auto transform hover:scale-110 duration-200"
-                    src={FlappyCatito}
-                    alt="Flappy Catito"
-                  />
-                </div>
-                <div className="p-7 my-auto pb-12 bg-cdln-blue-50">
-                  <h1 className="text-4xl font-semibold text-gray-800 mt-4">
-                    Flappy Catito
-                  </h1>
-                  <p className="text-lg md:text-2xl text-gray-400 mt-4 leading-relaxed">
-                    Ajude o Catito passarinho a atravessar os obstáculos de
-                    pernas e chegar no conforto de sua casa para jogar Nintendo
-                    Switch. Cuidado ao esbarrar!
-                  </p>
-                </div>
-                <a href="https://flappycatito.herokuapp.com/" target="_blank">
-                  <div className="flex gap-3 justify-center border-t py-8 items-center text-cdln-blue-200 group-hover:text-cdln-blue-50 bg-cdln-blue-600 hover:bg-cdln-blue-700">
-                    <IoGameController size={"1.5em"} />
-                    <h2 className="text-center text-xl tracking-widest uppercase">
-                      Jogar
-                    </h2>
-                  </div>
-                </a>
-              </article>
-              <article className="rounded-md overflow-hidden relative max-w-sm group shadow-md mx-auto transform hover:-translate-y-1 duration-300 hover:shadow-xl cursor-pointer">
-                <div className="overflow-hidden">
-                  <img
-                    className="w-full h-auto transform hover:scale-110 duration-200"
-                    src={LoraxKart}
-                    alt="Lorax Kart"
-                  />
-                </div>
-                <div className="p-7 my-auto pb-12 bg-cdln-blue-50">
-                  <h1 className="text-4xl font-semibold text-gray-800 mt-4">
-                    Lorax Kart
-                  </h1>
-                  <p className="text-lg md:text-2xl text-gray-400 mt-4 leading-relaxed">
-                    Aperte os cintos! Embarque com Lorax nesse grande desafio
-                    cheio de adrenalina na grande São Paulo e busque o Flávio no
-                    aeroporto de Guarulhos a tempo.
-                  </p>
-                </div>
-                <a href="https://lorax-kart.netlify.app/" target="_blank">
-                  <div className="hidden md:flex gap-3 justify-center border-t py-8 items-center text-cdln-blue-200 group-hover:text-cdln-blue-50 bg-cdln-blue-600 hover:bg-cdln-blue-700">
-                    <IoGameController size={"1.5em"} />
-                    <h2 className="text-center text-xl tracking-widest uppercase">
-                      Jogar
-                    </h2>
-                  </div>
-                  <div className="flex md:hidden gap-3 justify-center border-t py-8 items-center text-white bg-gray-500">
-                    <MdPhonelinkErase size={"1.5em"} />
-                    <h2 className="text-sm tracking-widest uppercase">
-                      Indisponível para dispositivos móveis
-                    </h2>
-                  </div>
-                </a>
-              </article>
-              <article className="rounded-md overflow-hidden relative max-w-sm group shadow-md mx-auto transform hover:-translate-y-1 duration-300 hover:shadow-xl cursor-pointer">
-                <div className="overflow-hidden">
-                  <img
-                    className="w-full h-auto transform hover:scale-110 duration-200"
-                    src={Akon}
-                    alt="Akon"
-                  />
-                </div>
-                <div className="p-7 my-auto pb-12 bg-cdln-blue-50">
-                  <h1 className="text-4xl font-semibold text-gray-800 mt-4">
-                    Jogo do AKON
-                  </h1>
-                  <p className="text-lg md:text-2xl text-gray-400 mt-4 leading-relaxed">
-                    Mentalize quem do grupo é o AKON. Descreva. Se prepare. E
-                    fique a tento a seu redor. Tenso, Emocionante e muita
-                    Adrenalina. <br />
-                    <strong>Apenas para dispositivos móveis</strong>
-                  </p>
-                </div>
-                <a href="https://youtu.be/mc5ub3TULqU?t=16" target="_blank">
-                  <div className="flex gap-3 justify-center border-t py-8 items-center text-cdln-blue-50 bg-gray-500">
-                    <h2 className="text-center text-xl tracking-widest uppercase">
-                      Em Breve
-                    </h2>
-                  </div>
-                </a>
-              </article>
+              <GameCard
+                image={FlappyCatito}
+                name="Flappy Catito"
+                description="Ajude o Catito passarinho a atravessar os obstáculos de pernas e chegar no conforto de sua casa para jogar Nintendo Switch. Cuidado ao esbarrar!"
+                gameUrl="https://flappycatito.herokuapp.com/"
+              />
+              <GameCard
+                image={LoraxKart}
+                name="Lorax Kart"
+                description="Aperte os cintos! Embarque com Lorax nesse grande desafio cheio de adrenalina na grande São Paulo e busque o Flávio no aeroporto de Guarulhos a tempo."
+                gameUrl="https://lorax-kart.netlify.app/"
+                available={true}
+              />
+              <GameCard
+                image={Akon}
+                name="Jogo do AKON"
+                description="Mentalize quem do grupo é o AKON. Descreva. Se prepare. E fique a tento a seu redor. Tenso, Emocionante e muita Adrenalina. <br /> <strong>Apenas para dispositivos móveis</strong>"
+                gameUrl="https://youtu.be/mc5ub3TULqU?t=16"
+                available={false}
+              />
             </div>
-            <h1 className="text-center font-semibold text-cdln-blue-50 text-2xl mb-5 lg:pb-0 md:text-5xl">Mais jogos incríveis em breve!</h1>
+            <h1 className="text-center font-semibold text-cdln-blue-50 text-2xl mb-5 lg:pb-0 md:text-5xl">
+              Mais jogos incríveis em breve!
+            </h1>
           </div>
         </div>
       </section>
