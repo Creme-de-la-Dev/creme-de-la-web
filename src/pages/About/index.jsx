3a793da0d3082c99db9e4ca097d342d8e1426971
gchan26 <guilhermechan30@gmail.com>
2023-05-11 14:00:21 -0300

    Created title

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 
 function About () {
     return (
-        <div className="app h-screen">
-            <h1>Creme de la Web About</h1>
+        <div>
+            <section className="header h-screen p-20">
+                <div className='flex-column max-w-4xl'>
+                    <h1 class="text-cdln-blue-100 text-7xl md:text-8xl font-bold animate__animated animate__pulse">Sobre Nós</h1>
+                    <h2 class="text-cdln-blue-50 text-xl md:text-3xl ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
+                </div>
+            </section>
         </div>
     )
 }
