3f3d0119b96b92c3488d6946574deb4bea59d151
gchan26 <guilhermechan30@gmail.com>
2023-05-16 15:07:26 -0300

    added link

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -19,7 +19,9 @@ function About() {
                 </h2>
                 <h2 class="mt-5 text-cdln-blue-50 text-lg lg:text-3xl animate__animated animate__fadeIn">Esses amigos têm uma conexão especial que transcende o tempo e a distância. Eles se encontram regularmente para celebrar aniversários, feriados e simplesmente para aproveitar a companhia uns dos outros. O Creme de la Nage valoriza o apoio mútuo, encorajando-se em seus sonhos e proporcionando um ombro amigo nos momentos difíceis. A cada encontro, a amizade se fortalece e se renova, mostrando que, mesmo ao longo dos anos, o laço do Creme de la Nage permanece inquebrável.</h2>
             </div>
+            <a href="https://www.youtube.com/watch?v=COSpT8Kp37k&list=FLm04vC3zTSn7QdjC_A_K-0g" target="_blank">
             <img src={Creme1} className="animate__animated animate__fadeIn mt-5 max-w-auto xl:max-w-xl border-sky-100 border-8 rounded-md"/>
+            </a>
         </div>
       </section>
     </div>
