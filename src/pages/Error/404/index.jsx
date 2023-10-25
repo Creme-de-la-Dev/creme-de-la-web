7b20faf9283f380b30faf3026581fd5622470211
gchan26 <guilhermechan30@gmail.com>
2023-10-24 23:27:10 -0300

    Created 404 page layout

--- src/pages/Error/404/index.jsx
+++ src/pages/Error/404/index.jsx
@@ -1,13 +1,38 @@
 // React
 import React from "react";
 
+// Images
+import Ale404 from "../../../assets/Errors/ale404.png";
+
+// Icons
+import { HiHome } from "react-icons/hi";
+
 const NotFound = () => {
-    return (
-        <div>
-            <h2>404 Not Found</h2>
-            <p>Sorry, the page you are looking for does not exist.</p>
+  return (
+    <div>
+      <section className="header min-h-screen w-full flex align-center">
+        <div className="flex align-center flex-col mx-auto">
+          <h2 className="text-center text-8xl mt-5 text-cdln-blue-100">
+            404 Not Found
+          </h2>
+          <img
+            src={Ale404}
+            className="m-5 max-w-auto xl:max-w-4xl border-sky-100 border-8 rounded-md self-center"
+          />
+          <p className="text-center text-cdln-blue-100 text-4xl">
+            Puts! Não foi possível encontrar a página que você estava
+            procurando!
+          </p>
+          <a href="/home" className="self-center">
+            <button className="m-5 bg-cdln-blue-50 hover:bg-cdln-blue-100 text-cdln-blue-800 font-bold py-4 px-6 border-2 border-cdln-blue-400 rounded-3xl shadow-lg self-center flex flex-row items-center gap-3 text-2xl max-w-xl">
+              <HiHome />
+              Retornar à Home
+            </button>
+          </a>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default NotFound;
