3db8b72d40a805f39e4e73e86f2023ed7e0d1130
gchan26 <guilhermechan30@gmail.com>
2023-11-09 11:45:30 -0300

    Fixed issue with mobile screens

--- src/components/Episodes/Episode.jsx
+++ src/components/Episodes/Episode.jsx
@@ -6,7 +6,7 @@ const Episode = ({ title, url, date, description, thumb }) => {
     <div className="episode flex flex-row items-center p-2 hover:bg-cdln-blue-800 hover:rounded-md">
       <a href={url} target="_blank" rel="noopener noreferrer" className='flex flex-col bg-cdln-blue-800 items-center p-2 md:p-0 rounded-md md:bg-transparent hover:bg-transparent md:flex-row text-white no-underline'>
         <div>
-          <img src={thumb} alt={title} className='rounded-md hover:shadow-md min-w-64' />
+          <img src={thumb} alt={title} className='mb-2 md:mb-0 rounded-md hover:shadow-md min-w-64' />
         </div>
         <div className='pl-5'>
           <h2 className='no-underline font-semibold hover:underline text-2xl'>{title}</h2>
