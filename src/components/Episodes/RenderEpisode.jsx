e31891d3350ef207ddd501c863240c77acf642c4
gchan26 <guilhermechan30@gmail.com>
2023-11-09 17:08:09 -0300

    trying to make page responsive

--- src/components/Episodes/RenderEpisode.jsx
+++ src/components/Episodes/RenderEpisode.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player/youtube";
-import NotFound from '../../pages/Error/404'
+import NotFound from "../../pages/Error/404";
 
 const RenderEpisode = ({ episodes }) => {
   const { episodeId } = useParams();
@@ -13,11 +13,31 @@ const RenderEpisode = ({ episodes }) => {
 
   return (
     <div>
-        <section className="header h-screen" id="main-section">
-            <h1>{selectedEpisode.title}</h1>
-            <ReactPlayer url={selectedEpisode.url} controls />
-            <p>{selectedEpisode.description}</p>
-        </section>
+      <section className="header min-h-screen p-6 w-full" id="main-section">
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-4xl md:text-7xl text-cdln-blue-50 font-medium py-4 text-center">
+            {selectedEpisode.title}
+          </h1>
+          <div className="flex flex-col items-center justify-center">
+            <div className="player-wrapper md:w-auto md:h-auto mx-auto">
+              <ReactPlayer
+                url={selectedEpisode.url}
+                controls
+                width={"1280px"}
+                height={"720px"}
+                className="react-player border-4 border-cdln-blue-200 rounded-lg"
+                
+              />
+            </div>
+            <p className="text-cdln-blue-100 text-sm md:text-xl">
+              {selectedEpisode.date}
+            </p>
+            <p className="text-cdln-blue-50 text-md md:text-2xl">
+              {selectedEpisode.description}
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
