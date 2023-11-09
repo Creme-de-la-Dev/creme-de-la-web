5b0d3759f3868ee1755e4332b39c9b300435c858
gchan26 <guilhermechan30@gmail.com>
2023-11-09 11:16:52 -0300

    Started creating responsive design

--- src/components/Episodes/Episode.jsx
+++ src/components/Episodes/Episode.jsx
@@ -4,14 +4,14 @@ import React from 'react';
 const Episode = ({ title, url, date, description, thumb }) => {
   return (
     <div className="episode flex flex-row items-center p-2 hover:bg-cdln-blue-800 hover:rounded-md">
-      <a href={url} target="_blank" rel="noopener noreferrer" className='flex flex-row text-white no-underline'>
+      <a href={url} target="_blank" rel="noopener noreferrer" className='flex flex-col bg-cdln-blue-800 items-center p-2 md:p-0 rounded-md md:bg-cdln-blue-900 md:flex-row text-white no-underline'>
         <div>
           <img src={thumb} alt={title} className='rounded-md hover:shadow-md min-w-64' />
         </div>
         <div className='pl-5'>
           <h2 className='no-underline font-semibold hover:underline text-2xl'>{title}</h2>
           <p className='text-cdln-blue-100 text-sm md:text-lg'>[{date}]</p>
-          <p>{description}</p>
+          <p className='text-md md:text-lg'>{description}</p>
         </div>
       </a>
     </div>
