eddd3b2066a36353a469a5e9bfd3af6d79689d36
gchan26 <guilhermechan30@gmail.com>
2024-04-30 20:49:16 -0300

    Fixing

--- src/components/Videos/RenderVideo.jsx
+++ src/components/Videos/RenderVideo.jsx
@@ -1,43 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ReactPlayer from "react-player/youtube";
-import { getSecretUrlByCode } from './videoUtils';
+import { getSecretUrlByCode } from "./videoUtils";
+import Logo from "../../assets/Logos/ale-logo.png";
 
 const RenderVideo = () => {
- const { code } = useParams();
+  const { code } = useParams();
+  const [videoUrl, setVideoUrl] = useState(null);
+  const [isInvalidCode, setIsInvalidCode] = useState(false);
 
- const videoUrl = getSecretUrlByCode(code);
+  useEffect(() => {
+    const fetchVideoUrl = async () => {
+      const url = await getSecretUrlByCode(code);
+      // Check if the returned URL is the default URL
+      if (url === "https://youtu.be/lLOBedFymoA?si=x1ifdiQ8LGUjqTdF&t=25") {
+        setIsInvalidCode(true);
+      } else {
+        setVideoUrl(url);
+      }
+    };
 
- return (
+    fetchVideoUrl();
+  }, [code]);
+
+  return (
     <div className="bg-black">
       <section className="header min-h-screen p-6 w-full" id="main-section">
         <div className="flex flex-col items-center justify-center h-full">
           <h1 className="text-4xl md:text-7xl text-cdln-blue-50 font-medium py-4 text-center">
-            {code}
+            {isInvalidCode ? "Código Inválido. Tente Novamente..." : code}
           </h1>
-          <div className="flex flex-col items-center justify-center">
-            <div className="player-wrapper md:w-auto md:h-auto mx-auto">
-              <ReactPlayer
-                url={videoUrl}
-                controls
-                width={"1280px"}
-                height={"720px"}
-                className="react-player border-4 border-cdln-blue-200 rounded-lg"
-                playing={true}
-              />
-            </div>
-            <div className="mt-8">
+          {isInvalidCode ? (
+            <div>
+              <div className="mt-8">
+                
                 <a href="/2209626308100203jodoboioxocoocofaiafbaabgoog">
-                <button class="ui-btn">
-                <span>Voltar</span>
-                </button>
-            </a>
+                  <button className="ui-btn">
+                    <span>Voltar</span>
+                  </button>
+                </a>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center">
+              <div className="player-wrapper md:w-auto md:h-auto mx-auto">
+                <ReactPlayer
+                  url={videoUrl}
+                  controls
+                  width={"1280px"}
+                  height={"720px"}
+                  className="react-player border-4 border-cdln-blue-200 rounded-lg"
+                  playing={true}
+                />
+              </div>
+              <div className="mt-8">
+                <a href="/2209626308100203jodoboioxocoocofaiafbaabgoog">
+                  <button className="ui-btn">
+                    <span>Voltar</span>
+                  </button>
+                </a>
+              </div>
+            </div>
+          )}
         </div>
       </section>
     </div>
- );
+  );
 };
 
 export default RenderVideo;
--- src/components/Videos/videoUtils.jsx
+++ src/components/Videos/videoUtils.jsx
@@ -2,11 +2,12 @@ export const secretUrls = {
     "AKON": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     "40NG": "https://www.youtube.com/watch?v=EXJwNBu4FP8",
     "CDLW": "https://www.youtube.com/watch?v=LSGti-GPSdU",
-    "LR2T": "https://www.youtube.com/watch?v=4OnZCdgO9YM"
+    "LR2T": "https://www.youtube.com/watch?v=4OnZCdgO9YM",
+    "@1BH": "https://www.youtube.com/watch?v=AZdHSGHQd2Q"
 
    };
    
    export function getSecretUrlByCode(code) {
-    return secretUrls[code] || "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // Default URL if code not found
+    return secretUrls[code] || "https://youtu.be/lLOBedFymoA?si=x1ifdiQ8LGUjqTdF&t=25";
    }
    
