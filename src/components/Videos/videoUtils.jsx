da1e5948965343c9c32ff1903ee11c2876d57457
gchan26 <guilhermechan30@gmail.com>
2024-05-01 00:31:39 -0300

    New secrets

--- src/components/Videos/videoUtils.jsx
+++ src/components/Videos/videoUtils.jsx
@@ -1,14 +1,14 @@
 export const secretUrls = {
     "----": "https://youtu.be/HEW9E0R1yn8?si=KThRCwhqK7sy1XtG&t=1",
 
-    "AKON": "https://www.youtube.com/watch?v=dQw4w9WgXcQ", //apk do akon
+    "AKON": "https://www.youtube.com/watch?v=vhp0Bz-IHZQ",
     "40NG": "https://www.youtube.com/watch?v=EXJwNBu4FP8",
     "LR2T": "https://www.youtube.com/watch?v=4OnZCdgO9YM",
     "@1BH": "https://youtu.be/xHNYJw1-1J8",
     "7!9*": "https://www.youtube.com/watch?v=dx_5Y1rah3E",
     "404!": "https://youtu.be/deYJua718GI",
     "BGS!": "https://www.youtube.com/watch?v=v-9lVVxkPtI",
-
+    "*DG4": "https://www.youtube.com/watch?v=FmW-EjUd5Ms",
    };
    
    export function getSecretUrlByCode(code) {
