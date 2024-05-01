faae287e2bfed0e1e670b5e0858f7eaa450d8003
gchan26 <guilhermechan30@gmail.com>
2024-05-01 00:57:36 -0300

    Created video data

---
+++ src/pages/Shows/CremeDeLaNageTV/index.jsx
@@ -0,0 +1,121 @@
+// React
+import React, { useEffect, useState } from "react";
+// import ReactPlayer from "react-player/youtube";
+
+// React Routes
+import { Link, useNavigate } from "react-router-dom";
+
+// Components
+import EpisodesList from "../../../components/Episodes/EpisodesList";
+import videoList from "../LuansRevolution/videoData";
+
+// Images
+import LRBG from "../../../assets/BackgroundImages/LRBG.png";
+import ShowsBGsm from "../../../assets/BackgroundImages/ShowsBG-sm.png";
+import ArrowDown from "../../../assets/arrow-down.svg";
+
+function CDLNTV() {
+  const [activeList, setActiveList] = useState("Temporada 1");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const navigate = useNavigate();
+
+  const navigateToEpisode = (url) => {
+    navigate(url);
+  };
+
+  const toggleList = (listName) => {
+    setActiveList(listName);
+    setIsDropdownOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div>
+      <section className="header h-screen">
+        <div className="h-full relative">
+          <img
+            src={LRBG}
+            className="hidden lg:block w-full h-full object-cover absolute"
+          />
+          <img
+            // Mudar foto para uma versão responsiva depois
+            src={LRBG}
+            className="lg:hidden w-full h-full object-cover absolute"
+          />
+          <div className="p-24 w-full h-full justify-center items-center">
+            <h1 className="text-cdln-blue-50 text-8xl md:text-9xl font-bold text-center animate__animated animate__jackInTheBox">
+              Luan's Revolution
+            </h1>
+            <h2 className="text-cdln-blue-100 text-2xl md:text-5xl font-normal mt-5 text-center animate__animated animate__slideInUp">
+              O combate aos frenes vai começar!
+            </h2>
+            <div className="w-full justify-center items-center pt-20 animate__animated animate__fadeIn">
+              <a href="#main-section">
+                <img src={ArrowDown} className="mx-auto h-10 w-10" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="main-section">
+        <div className="hero h-auto lg:min-h-screen p-10">
+          <h1 className="text-4xl md:text-7xl text-cdln-blue-50 font-medium pb-4">
+            Lista de Episódios
+          </h1>
+          <button
+            onClick={toggleDropdown}
+            id="dropdownDefaultButton"
+            data-dropdown-toggle="dropdown"
+            className="text-white bg-cdln-blue-600 hover:bg-cdln-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center"
+            type="button"
+          >
+            {activeList ?? "Selecionar Temporada"}
+            <svg
+              className={`w-2.5 h-2.5 ml-2.5 ${
+                isDropdownOpen ? "transform rotate-180" : ""
+              }`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 4 4 4-4"
+              />
+            </svg>
+          </button>
+
+          {isDropdownOpen && (
+            <div className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute mt-2">
+              <ul className="py-2 text-lg text-gray-700 dark:text-gray-200">
+                {videoList.map((listName) => (
+                  <li key={listName.title}>
+                    <a
+                      href="#main-section"
+                      onClick={() => toggleList(listName.title)}
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      {listName.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+        <EpisodesList episodes={videoList.find((v) => v.title === activeList).videos} />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default CDLNTV;
---
+++ src/pages/Shows/CremeDeLaNageTV/videoData.jsx
@@ -0,0 +1,123 @@
+const videoList = [
+    {
+      title: "Creme de la Nage TV",
+      videos: [
+        {
+          title: "Episódio 1",
+          id: "cdlntv1",
+          url: "https://youtu.be/etSGRCco-Po",
+          date: "13/01/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/L4rrvv3C/image-26.png",
+        },
+        {
+          title: "Episódio 2",
+          id: "cdlntv2",
+          url: "https://youtu.be/0f7Sev9DHlY",
+          date: "14/01/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/qq1rBBMv/KRAFTPUNK.png",
+        },
+        {
+          title: "Episódio 3",
+          id: "cdlntv3",
+          url: "https://youtu.be/g-OIr0mxv_4",
+          date: "14/01/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/mgSJL4fJ/image-28.png",
+        },
+        {
+          title: "Episódio 4",
+          id: "cdlntv4",
+          url: "https://youtu.be/1bwAeikCpdA",
+          date: "22/01/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/V6V2kzSY/abetterplace.png",
+        },
+        {
+          title: "Episódio 5",
+          id: "cdlntv5",
+          url: "https://youtu.be/JPFwXP6LfXk",
+          date: "01/02/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/GphN9fb2/image-30.png",
+        },
+        {
+          title: "Episódio 6",
+          id: "cdlntv6",
+          url: "https://youtu.be/nxHXtGzzOIo",
+          date: "24/02/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/kGckxWGy/thumbcrossover.png",
+        },
+        {
+          title: "Episódio 7",
+          id: "cdlntv7",
+          url: "https://youtu.be/mA5kMjtLNgk",
+          date: "26/02/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/fyXq6nfH/image-32.png",
+        },
+        {
+          title: "Episódio 8",
+          id: "cdlntv8",
+          url: "https://youtu.be/zBgOudXD-n8",
+          date: "08/10/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/cHP594wg/image-33.png",
+        },
+        {
+          title: "Episódio 9",
+          id: "cdlntv9",
+          url: "https://youtu.be/uh63yocPP40",
+          date: "15/12/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/ZRWMxyBz/image-34.png",
+        },
+        {
+          title: "Episódio 10",
+          id: "cdlntv10",
+          url: "https://youtu.be/h4bVAh6zE74",
+          date: "07/01/2017",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/BZ1z8Hfx/image-35.png",
+        },
+      ],
+    },
+    {
+      title: "Extras",
+      videos: [
+        {
+          title: "[TEASER] - Temporada 2'",
+          id:"cdlntvex1",
+          url: "https://youtu.be/_G6dkHpscvM",
+          date: "29/09/2016",
+          description:
+            "",
+          thumb: "https://i.postimg.cc/pVF4Qhpv/ballas.png",
+        },
+        {
+          title: "[TRAILER] - Temporada 2",
+          id:"cdlntvex2",
+          url: "https://youtu.be/V76v0khT8Uk",
+          date: "03/10/2016",
+          description: "",
+          thumb: "https://i.postimg.cc/bY4TT34V/teaser.png",
+        },
+       
+      ],
+    },
+  ];
+  
+  export default videoList;
+  
