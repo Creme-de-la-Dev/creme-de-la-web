25ce95d5f9bdb9d8f88e77a672434bea07c31b92
gchan26 <guilhermechan30@gmail.com>
2024-08-08 15:01:20 -0300

    Created initial blog file

---
+++ src/pages/Blog/index.jsx
@@ -0,0 +1,13 @@
+// React
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Blog = () => {
+  return (
+		<div className="h-screen">
+            
+		</div>
+	)
+}
+
+export default Blog;
