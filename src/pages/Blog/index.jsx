b53f539ba5d1c9a7fad5e1cef6e3b89af51534ff
gchan26 <guilhermechan30@gmail.com>
2024-08-08 17:07:48 -0300

    Created first blog post

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,12 +1,29 @@
 // React
-// eslint-disable-next-line no-unused-vars
 import React from "react";
+
+import PostIMG from "../../assets/Blog/1.0.1.png"
 
 const Blog = () => {
   return (
-		<div className="h-screen">
-            
-		</div>
+        <>
+            <section className="min-h-screen container w-full flex flex-col items-center mx-auto p-5">
+                <div className="bg-white h-full w-full rounded-lg p-10">
+                    <div className="text-cdln-blue-800 flex flex-col gap-5">
+                        <h1 className="text-5xl font-semibold text-cdln-blue-900">Versão 1.0.1 lançada!</h1>
+                        <h6 className="text-xl">03/05/2024</h6>
+                        <img src={PostIMG} className="rounded-lg border-2 max-w-lg"/>
+                        <p className="text-xl font-medium">
+                            Nesta atualização:<br />
+                            - Lorax Kart portado 100% para dentro do site;<br/>
+                            - Creme de la Nage TV portado 100% para dentro do site (sem susto dessa vez);<br/>
+                            - Correção de bugs de usabilidade e layout;<br/>
+                            - Adicionados 10 Easter Eggs secretos.
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+   
 	)
 }
 
