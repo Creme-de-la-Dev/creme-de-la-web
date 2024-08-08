fe4c1bf6b4db16e74045f7bf2c3f3e0398c27f19
gchan26 <guilhermechan30@gmail.com>
2024-08-08 17:26:33 -0300

    Fixed image layout

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -7,11 +7,11 @@ const Blog = () => {
   return (
         <>
             <section className="min-h-screen container w-full flex flex-col items-center mx-auto p-5">
-                <div className="bg-white h-full w-full rounded-lg p-10">
+                <div className="bg-white h-full rounded-lg p-10">
                     <div className="text-cdln-blue-800 flex flex-col gap-5">
                         <h1 className="text-5xl font-semibold text-cdln-blue-900">Versão 1.0.1 lançada!</h1>
                         <h6 className="text-xl">03/05/2024</h6>
-                        <img src={PostIMG} className="rounded-lg border-2 max-w-lg"/>
+                        <img src={PostIMG} className="rounded-lg border-2 w-72 md:w-96 max-w-lg"/>
                         <p className="text-xl font-medium">
                             Nesta atualização:<br />
                             - Lorax Kart portado 100% para dentro do site;<br/>
