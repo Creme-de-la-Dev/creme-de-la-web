554d9353027797fbf89652b5b1c9fc49110cb850
gchan26 <guilhermechan30@gmail.com>
2024-08-09 14:53:06 -0300

    separated posts and added divider

--- src/pages/Blog/postsData.jsx
+++ src/pages/Blog/postsData.jsx
@@ -15,4 +15,18 @@ export const posts = [
       </>
     ),
   },
+  {
+    title: "Versão 1.0.2 lançada!",
+    date: "03/05/2024",
+    image: PostIMG,
+    content: (
+      <>
+        Nesta atualização:<br />
+        - Lorax Kart portado 100% para dentro do site;<br />
+        - Creme de la Nage TV portado 100% para dentro do site (sem susto dessa vez);<br />
+        - Correção de bugs de usabilidade e layout;<br />
+        - Adicionados 10 Easter Eggs secretos.
+      </>
+    ),
+  },
 ];
