b849c9e3394e475aed164b1e5da97992fffa84e9
gchan26 <guilhermechan30@gmail.com>
2025-10-24 18:38:19 -0300

    Create Blog page

--- src/components/Blog/BlogPost.jsx
+++ src/components/Blog/BlogPost.jsx
@@ -4,7 +4,9 @@ const BlogPost = ({ title, date, image, content }) => {
   return (
     <div className="text-cdln-blue-800 flex flex-col gap-5">
       <h1 className="text-5xl font-semibold text-cdln-blue-900">{title}</h1>
-      <h6 className="text-xl">{date}</h6>
+      <h6 className="text-xl flex items-center gap-2">
+        📅 {date}
+      </h6>
       <img src={image} className="rounded-lg border-2 w-72 md:w-96 max-w-lg" alt={title} />
       <p className="text-xl font-medium">{content}</p>
     </div>
--- src/pages/Blog/postsData.jsx
+++ src/pages/Blog/postsData.jsx
@@ -2,7 +2,7 @@ import v101 from "../../assets/Blog/1.0.1.png";
 
 export const posts = [
   {
-    title: "Versão 1.0.1 lançada!",
+    title: "Versão 1.1 lançada!",
     date: "03/05/2024",
     image: v101,
     content: (
@@ -17,16 +17,14 @@ export const posts = [
   },
 
 //   {
-//     title: "Versão 1.0.2 lançada!",
-//     date: "??/??/2024",
+//     title: "Versão 1.2 lançada!",
+//     date: "31/10/2025",
 //     image: PostIMG,
 //     content: (
 //       <>
 //         Nesta atualização:<br />
-//         - Lorax Kart portado 100% para dentro do site;<br />
-//         - Creme de la Nage TV portado 100% para dentro do site (sem susto dessa vez);<br />
 //         - Correção de bugs de usabilidade e layout;<br />
-//         - Adicionados 10 Easter Eggs secretos.
+//         - Adicionados novos 10 códigos secretos.
 //       </>
 //     ),
 //   },
